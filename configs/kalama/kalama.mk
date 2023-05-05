402cf6a56df303b8f8432ff93788112ab44a5c81
Sourajit Karmakar <sourajit@live.com>
2023-05-05 17:10:40 +0000

    configs: Drop a visibly annoying warning when GENERIC_ODM_IMAGE is set Change-Id: I6ce0c8ede381a6a148413ede05254154499dbc58 Reviewed-on: https://review.statixos.com/c/android_hardware_qcom_audio/+/7989 Reviewed-by: Sourajit Karmakar <sourajit@live.com> Tested-by: Sourajit Karmakar <sourajit@live.com> Signed-off-by: chandu078 <chandudyavanapelli03@gmail.com>

--- configs/kalama/kalama.mk
+++ configs/kalama/kalama.mk
@@ -382,7 +382,6 @@ vendor.audio.compress_capture.aac=true
 # compress capture end
 
 ifneq ($(GENERIC_ODM_IMAGE),true)
-$(warning "Enabling codec2.0 SW only for non-generic odm build variant")
 #Rank OMX SW codecs lower than OMX HW codecs
 PRODUCT_VENDOR_PROPERTIES += debug.stagefright.omx_default_rank=0
 endif
